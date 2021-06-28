ad6fc870ded3ad97132e3ee4c50575b35c1ca781
Jaison Mora <jaison.mora@mcets-inc.com>
2021-06-28 00:15:58 -0500

    Initial Commit

---
+++ app/config/services.php
@@ -0,0 +1,69 @@
+<?php
+declare(strict_types=1);
+
+use Phalcon\Mvc\View;
+use Phalcon\Mvc\View\Engine\Volt as VoltEngine;
+use Phalcon\Url as UrlResolver;
+use Phalcon\Db\Adapter\Pdo\MySQL;
+
+/**
+ * Shared configuration service
+ */
+$di->setShared('config', function () {
+    return include APP_PATH . "/config/config.php";
+});
+
+/**
+ * The URL component is used to generate all kind of urls in the application
+ */
+$di->setShared('url', function () {
+    $config = $this->getConfig();
+
+    $url = new UrlResolver();
+    $url->setBaseUri($config->application->baseUri);
+
+    return $url;
+});
+
+/**
+ * Setting up the view component
+ */
+$di->setShared('view', function () {
+    $config = $this->getConfig();
+
+    $view = new View();
+    $view->setDI($this);
+    $view->setViewsDir($config->application->viewsDir);
+
+    $view->registerEngines([
+        '.volt' => function ($view) {
+            $config = $this->getConfig();
+
+            $volt = new VoltEngine($view, $this);
+
+            $volt->setOptions([
+                'path' => $config->application->cacheDir,
+                'separator' => '_'
+            ]);
+
+            return $volt;
+        }
+    ]);
+
+    return $view;
+});
+
+/**
+ * Database connection is created based in the parameters defined in the configuration file
+ */
+$di->setShared('db', function () {
+    $config = $this->getConfig();
+
+    return new Mysql([
+        'host'     => $config->database->host,
+        'username' => $config->database->username,
+        'password' => $config->database->password,
+        'dbname'   => $config->database->dbname,
+        'charset'  => $config->database->charset
+    ]);
+});
